6d108b388f2696bd4b6472991cfdf0ed0c29696e
Dmytro Storchak <dmitriystorchak98@gmail.com>
2024-03-24 16:32:30 +0200

    realize task2

---
+++ Task2.py
@@ -0,0 +1,61 @@
+def additional(num1, num2):
+    """Adds two numbers"""
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    """Subtracts two numbers"""
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    """Multiplies two numbers"""
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """Divides two numbers"""
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        return "Division by zero error"
+
+
+def power(num1, num2):
+    """Powers two numbers"""
+    try:
+        if num1 == 0 and num2 < 0:
+            raise ValueError("You can't raise zero to a negative power")
+        return num1 ** num2
+    except Exception as e:
+        return f"ValueError, {e}"
+
+
+def menu():
+    """Menu options"""
+    while True:
+        number1 = int(input("Enter first number: "))
+        number2 = int(input("Enter second number: "))
+
+        print("1. additional\n2. subtraction\n3. multiplication\n4. division\n5. power\n6. exit")
+        choice = input("Select the operation: ").strip()
+
+        match choice:
+            case "1":
+                print(additional(number1, number2))
+            case "2":
+                print(subtract(number1, number2))
+            case "3":
+                print(multiply(number1, number2))
+            case "4":
+                print(divide(number1, number2))
+            case "5":
+                print(power(number1, number2))
+            case "6":
+                break
+            case _:
+                print("Incorrect input, please try again!")
+
+
+if __name__ == '__main__':
+    menu()
